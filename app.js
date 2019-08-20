b06129334482bd2b82692d86c981f2dc4b3a03fd
KristinaStepanova <kri18stina@gmail.com>
2019-08-20 16:06:46 +0300

    edits

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // Форма
 // Список задач
 const tasks = [
-  {
-    _id: '5d2ca9e2e03d40b326596aa7',
-    completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: 'Eu ea incididunt sunt consectetur fugiat nonOccaecat non ea quis occaecat ad culpa.',
-  },
-  {
-    _id: '5d2ca9e29c8a94095c4e88e0',
-    completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  },
+  // {
+  //   _id: '5d2ca9e2e03d40b326596aa7',
+  //   completed: true,
+  //   body:
+  //     'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+  //   title: 'Eu ea incididunt sunt consectetur fugiat nonOccaecat non ea quis occaecat ad culpa.',
+  // },
+  // {
+  //   _id: '5d2ca9e29c8a94095c4e88e0',
+  //   completed: false,
+  //   body:
+  //     'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+  //   title:
+  //     'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  // },
 ];
 
 (function (arrOfTasks) {
@@ -31,16 +31,10 @@ const tasks = [
   const form = document.forms['addTask'];
   const inputTitle = form.elements['title'];
   const inputBody = form.elements['body'];
-  let message;
+  const message = document.querySelector('.message');
 
-  if (tasks.length === 0) {
-    message = document.createElement('li');
-    message.textContent = "No tasks";
-    message.classList.add(
-      'list-group-item',
-      'text-center',
-    );
-    tasksList.appendChild(message);
+  if (tasks.length !== 0) {
+    message.style.display = 'none';
   }
 
   renderTasks();
